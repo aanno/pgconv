923af57f1fdf978f572f6ca22cc9297fb7c01291
aanno <aanno@users.noreply.github.com>
2023-07-30 19:07:21 +0200

    fixHref

--- src/main/kotlin/impl/Readability4jExt.kt
+++ src/main/kotlin/impl/Readability4jExt.kt
@@ -3,6 +3,7 @@ package org.github.aanno.pgconv.impl
 import impl.MetaTags
 import net.dankito.readability4j.Article
 import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
 
 fun Article.getContentWithEncodingAsElement(encoding: String): Document {
     /*
@@ -23,5 +24,20 @@ fun Article.getContentWithEncodingAsElement(encoding: String): Document {
 fun Article.getContentWithEncodingAsElement(encoding: String, metaTags: MetaTags): Document {
     val document = getContentWithEncodingAsElement(encoding)
     metaTags.writeMeta(document)
+    fixHref(document)
     return document
+}
+
+private fun fixHref(element: Element) {
+    element.select("a").forEach {
+        val v = it.attr("href")
+        it.attr("href", fixHref(v))
+    }
+}
+
+private fun fixHref(string: String): String {
+    if (string.startsWith("file://null/")) {
+        return string.substring(12)
+    }
+    return string
 }
